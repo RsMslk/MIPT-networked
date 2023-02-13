d49f5822deffdb739f456bbb08d90921ae4f137a
RsMslk <masliuk.r@phystech.edu>
2023-02-13 16:15:49 +0300

    HW_1

--- w1/client.cpp
+++ w1/client.cpp
@@ -7,27 +7,85 @@
 #include <iostream>
 #include "socket_tools.h"
 
-int main(int argc, const char **argv)
+#include <fstream>
+#include <unistd.h>
+#include <thread>
+
+void Listen(int sfd)
 {
-  const char *port = "2022";
-
-  addrinfo resAddrInfo;
-  int sfd = create_dgram_socket("localhost", port, &resAddrInfo);
-
-  if (sfd == -1)
-  {
-    printf("Cannot create a socket\n");
-    return 1;
-  }
-
   while (true)
   {
-    std::string input;
-    printf(">");
-    std::getline(std::cin, input);
-    ssize_t res = sendto(sfd, input.c_str(), input.size(), 0, resAddrInfo.ai_addr, resAddrInfo.ai_addrlen);
-    if (res == -1)
-      std::cout << strerror(errno) << std::endl;
+    fd_set readSet;
+    FD_ZERO(&readSet);
+    FD_SET(sfd, &readSet);
+
+    timeval timeout = { 0, 100000 }; // 100 ms
+    select(sfd + 1, &readSet, NULL, NULL, &timeout);
+
+
+    if (FD_ISSET(sfd, &readSet))
+    {
+      constexpr size_t buf_size = 1000;
+      static char buffer[buf_size];
+      memset(buffer, 0, buf_size);
+
+      ssize_t numBytes = recvfrom(sfd, buffer, buf_size - 1, 0, nullptr, nullptr);
+      if (numBytes > 0)
+        printf("%s\n", buffer); // assume that buffer is a string
+    }
   }
+}
+
+
+int main(int argc, const char **argv)
+{
+  std::string input;
+  std::cin >> input;
+  const char *send_port = "2023";
+  const char *recv_port = "2022";
+
+  addrinfo resAddrInfo;
+  int sfd = create_dgram_socket("localhost", send_port, &resAddrInfo);
+  int sfd_recv = create_dgram_socket(nullptr, recv_port, nullptr);
+  if (sfd == -1)
+  {
+    printf("Cannot create a socket_send\n");
+    return 1;
+  }
+  if (sfd_recv == -1)
+  {
+    printf("Cannot create a socket_recv\n");
+    return 1;
+  }
+  ssize_t res;
+  
+  if (input =="init")
+  {
+    std::string hello = "Hello? From client";
+    res = sendto(sfd, hello.c_str(), hello.size(), 0, resAddrInfo.ai_addr, resAddrInfo.ai_addrlen);
+    printf("Hello msg sent\n");
+  }
+
+  std::thread listen_thread(Listen, sfd_recv);
+  listen_thread.detach();
+
+  std::ifstream in("./send_text.txt");
+  if (in.is_open())
+  {
+    while (std::getline(in, input))
+    {
+      printf(">");
+      res = sendto(sfd, input.c_str(), input.size(), 0, resAddrInfo.ai_addr, resAddrInfo.ai_addrlen);
+      sleep(10);
+      if (res == -1)
+        std::cout << strerror(errno) << std::endl;
+    }
+  }
+  else  
+  {
+    printf("Can't open input file");
+  }
+  in.close();
+  
   return 0;
 }
--- w1/server.cpp
+++ w1/server.cpp
@@ -6,17 +6,11 @@
 #include <cstdio>
 #include <iostream>
 #include "socket_tools.h"
+#include <thread>
+#include <time.h>
 
-int main(int argc, const char **argv)
+int init(int sfd)
 {
-  const char *port = "2022";
-
-  int sfd = create_dgram_socket(nullptr, port, nullptr);
-
-  if (sfd == -1)
-    return 1;
-  printf("listening!\n");
-
   while (true)
   {
     fd_set readSet;
@@ -35,8 +29,88 @@ int main(int argc, const char **argv)
 
       ssize_t numBytes = recvfrom(sfd, buffer, buf_size - 1, 0, nullptr, nullptr);
       if (numBytes > 0)
+      {
         printf("%s\n", buffer); // assume that buffer is a string
+        return 1;
+        break;
+      }
     }
   }
+}
+
+void Listen(int sfd, int sfd_send, addrinfo resAddrInfo)
+{
+  ssize_t res;
+  clock_t start, stop;
+  start = clock ();
+  std::string input;
+  while (true)
+  {
+    fd_set readSet;
+    FD_ZERO(&readSet);
+    FD_SET(sfd, &readSet);
+
+    timeval timeout = { 0, 100000 }; // 100 ms
+    select(sfd + 1, &readSet, NULL, NULL, &timeout);
+
+
+    if (FD_ISSET(sfd, &readSet))
+    {
+      constexpr size_t buf_size = 1000;
+      static char buffer[buf_size];
+      memset(buffer, 0, buf_size);
+
+      ssize_t numBytes = recvfrom(sfd, buffer, buf_size - 1, 0, nullptr, nullptr);
+      if (numBytes > 0)
+      {
+        printf("%s\n", buffer); // assume that buffer is a string
+        stop = clock();
+        input = std::to_string((double) (stop - start));
+        res = sendto(sfd_send, input.c_str(), input.size(), 0, resAddrInfo.ai_addr, resAddrInfo.ai_addrlen);
+      }
+    }
+  }
+}
+
+int main(int argc, const char **argv)
+{
+  const char *recv_port = "2023";
+  const char *send_port = "2022";
+  addrinfo resAddrInfo;
+  int sfd = create_dgram_socket(nullptr, recv_port, nullptr);
+  int sfd_send = create_dgram_socket("localhost", send_port, &resAddrInfo);
+
+  if (sfd == -1)
+  {
+    printf("Cannot create a socket_recv\n");
+    return 1;
+  }
+  if (sfd_send == -1)
+  {
+    printf("Cannot create a socket_send\n");
+    return 1;
+  }
+  fd_set readSet;
+  FD_ZERO(&readSet);
+  FD_SET(sfd, &readSet);
+
+  timeval timeout = { 0, 100000 }; // 100 ms
+  select(sfd + 1, &readSet, NULL, NULL, &timeout);
+  int initiated = 0;
+  initiated = init(sfd);
+  if(initiated == 1)
+  {
+    std::string input = "Hello";
+    ssize_t res;
+    res = sendto(sfd_send, input.c_str(), input.size(), 0, resAddrInfo.ai_addr, resAddrInfo.ai_addrlen);
+    printf("Hello msg sent\n");
+    if (res == -1)
+      std::cout << strerror(errno) << std::endl;
+    printf("Listening!\n");
+    Listen(sfd, sfd_send, resAddrInfo);
+  }
+
+
+  
   return 0;
 }
